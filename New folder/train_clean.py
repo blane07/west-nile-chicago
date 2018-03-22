b4eee556fef64464c404f5a3749aa933059f9adc
blane07 <brendan.lane07@gmail.com>
2018-03-22 16:55:05 -0400

    Cleanned up folders and achieved used documents

--- New folder/train_clean.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar  3 10:15:35 2018
-
-@author: moisessalazar77
-"""
-
-file_path2='C:\\Users\moisessalazar77\\Desktop\\west_nile_virus\\west_nile\\input\\train.csv'
-train=pd.read_csv(file_path2)
